f1a1df824795c05ff77a6ba77193ea5ac126725b
DmytroHavryliak96 <dmytro.havryliak@gmail.com>
2016-09-28 22:07:42 +0300

    Добавьте файлы проекта.

---
+++ lab3/Distribution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public static class Distribution
+    {
+        public static double[] Exponential(double min, double max, double rate, int count)
+        {
+            double x;
+            double[] array = new double[count];
+            Random rdn = new Random();
+            for(int i = 0; i < count; i++)
+            {
+
+                x = Math.Log(rdn.NextDouble()) * (-1 / rate) * (max - min) + min;
+                array[i] = x;
+             
+            }
+    
+            return array;
+        }
+
+        public static double[] Normal(double min, double max, int count)
+        {
+            double x;
+            double mean = (max + min) / 2.0;
+            double stdev = (mean - min) / 3.000;
+            double[] array = new double[count];
+            Random rdn = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                x = (rdn.NextDouble() * stdev) + mean;
+                array[i] = x;             
+            }
+            return array;
+                
+        }
+    }
+}
